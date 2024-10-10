3f34dd157e37aabb23995ac49fc8315920910c13
IabSDocker <llswdhh@163.com>
2024-10-10 12:44:03 +0800

    Update check-version.sh

--- check-version.sh
+++ check-version.sh
@@ -11,7 +11,7 @@ for i in $(seq 108 ${SEARCH_PAGES}); do
     curl -s "https://packages.gitlab.com/app/gitlab/gitlab-ce/search?dist=&filter=debs&page=${i}&q=" | grep "_arm64.deb" | grep -v '\-rc' | sed 's/.*>\(.*\)<.*/\1/' | sort -u | sed 's/gitlab-ce_\(.*\)_arm64.deb/\1/' >> version_list;
 done
 
-# grep -v '^\(1[0-3]\)\.' version_list | sort -Vu version_list -o version
+grep -v '^\(1[0-3]\)\.' version_list | sort -Vu -o version
 
 echo "Latest versions:"
 cat version_list
