486ceef810b06635b1e434ccf9455490c19e7826
IabSDocker <llswdhh@163.com>
2024-10-11 13:29:28 +0800

    Update check-version.sh

--- check-version.sh
+++ check-version.sh
@@ -10,7 +10,7 @@ fi
 
 # 76
 
-for i in $(seq 9 ${SEARCH_PAGES}); do
+for i in $(seq 7 ${SEARCH_PAGES}); do
     curl -s "https://packages.gitlab.com/app/gitlab/gitlab-ce/search?dist=&filter=debs&page=${i}&q=" | grep "_arm64.deb" | grep -v '\-rc' | sed 's/.*>\(.*\)<.*/\1/' | sort -u | sed 's/gitlab-ce_\(.*\)_arm64.deb/\1/' >> version_list;
 done
 
